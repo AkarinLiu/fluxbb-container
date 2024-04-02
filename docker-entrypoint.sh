7a6d650c3f487029731d77d2860975d1658dd1a4
AkarinLiu <akarinliu@outlook.com>
2024-04-02 16:07:19 +0800

    修改提示语

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 if [ -e ! /var/www/html/index.php ]; then
-    echo "Extracting files..."
+    echo "Copying files..."
     cp -r /data/* /var/www/html/
     chown -R www-data:www-data /var/www/html/
     chmod -R 755 /var/www/html/
-    echo "Files extracted."
 fi
 echo "Starting Fluxbb..."
 httpd -D FOREGROUND
